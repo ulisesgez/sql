e69adf83357c9f514d1d397779774feced0f7fad
ulisesGez <gu362937@uaeh.edu.mx>
2024-02-18 19:51:38 -0600

    comments

---
+++ home/aaa_introduccion.sql
@@ -0,0 +1,77 @@
+/*
+Bases de Datos:
+Es un conjunto de datos almacenados en un sistema de computadora, que pueden
+ser consultados y actualizados de manera simultánea por varios usuarios.
+
+Datos:
+Son hechos conocidos que pueden ser registrados y que tienen un significado
+implícito.
+
+Información:
+Es el resultado del procesamiento de los datos.
+
+Sistema de Gestión de Bases de Datos (SGBD):
+Es un conjunto de programas que permiten el almacenamiento, modificación y
+extracción de la información en una base de datos.
+
+Tipos de Bases de Datos:
+- Jerárquicas
+- Red
+- Relacionales
+- Orientadas a Objetos
+- NoSQL
+
+Modelo Relacional:
+Es un modelo de datos que representa la información en forma de tablas.
+
+Tablas:
+Son estructuras de datos que almacenan la información en filas y columnas.
+
+Filas:
+Son los registros de la tabla.
+
+Columnas:
+Son los campos de la tabla.
+
+Clave Primaria:
+Es un campo o conjunto de campos que identifican de manera única a cada
+registro de la tabla.
+
+Clave Foránea:
+Es un campo o conjunto de campos que hacen referencia a la clave primaria de
+otra tabla.
+
+SQL (Structured Query Language):
+Es un lenguaje de programación que se utiliza para gestionar bases de datos
+relacionales.
+
+DDL (Data Definition Language):
+Es un subconjunto de SQL que se utiliza para definir la estructura de la base
+de datos.
+
+DML (Data Manipulation Language):
+Es un subconjunto de SQL que se utiliza para manipular los datos de la base de
+datos.
+
+DCL (Data Control Language):
+Es un subconjunto de SQL que se utiliza para controlar el acceso a los datos.
+
+TCL (Transaction Control Language):
+Es un subconjunto de SQL que se utiliza
+para controlar las transacciones en la base de datos.
+
+Comentarios:
+Son textos que se utilizan para documentar el código fuente de un programa.
+
+Comentario de una línea:
+Se utiliza para documentar una línea de código.
+
+Comentario de varias líneas:
+Se utiliza para documentar varias líneas de código.
+
+Comentario de bloque:
+Se utiliza para documentar un bloque de código.
+
+Comentario de documentación:
+Se utiliza para documentar un programa.
+*/
