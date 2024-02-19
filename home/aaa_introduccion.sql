5c49365ab030dd3be4b6eef65396191a2830acfe
ulisesGez <gu362937@uaeh.edu.mx>
2024-02-19 17:17:17 -0600

    dataBases

--- home/aaa_introduccion.sql
+++ home/aaa_introduccion.sql
@@ -104,4 +104,89 @@ manipulación de datos en una base de datos:
 - Read
 - Update
 - Delete
+
+Logica de Negocio:
+Es el conjunto de reglas y procedimientos que definen el comportamiento de un
+sistema de información.
+
+Llaves:
+Son los campos que se utilizan para relacionar las tablas de una base de datos.
+
+Llave Primaria:
+Es un campo o conjunto de campos que identifican de manera única a cada
+registro de la tabla.
+
+Llave Foránea:
+Es un campo o conjunto de campos que hacen referencia a la clave primaria de
+otra tabla.
+
+Llave Unica:
+Es un campo o conjunto de campos que identifican de manera única a cada
+registro de la tabla.
+
+Llave Candidata:
+Es un campo o conjunto de campos que pueden ser utilizados como clave primaria.
+
+Llave Compuesta:
+Es un conjunto de campos que se utilizan como clave primaria.
+
+Llave Natural:
+Es un campo o conjunto de campos que se utilizan como clave primaria y que
+tienen un significado en el mundo real.
+
+Llave Sintética:
+Es un campo o conjunto de campos que se utilizan como clave primaria y que no
+tienen un significado en el mundo real.
+
+Relaciones:
+Son las asociaciones entre las entidades.
+
+Relación Uno a Uno:
+Es una relación en la que un registro de una tabla se relaciona con un solo
+registro de otra tabla.
+
+Relación Uno a Muchos:
+Es una relación en la que un registro de una tabla se relaciona con varios
+registros de otra tabla.
+
+Relación Muchos a Muchos:
+Es una relación en la que varios registros de una tabla se relacionan con
+varios registros de otra tabla.
+
+Modelo Entidad-Relación:
+Es un modelo de datos que representa la información en forma de entidades y
+relaciones.
+
+Modelo Relacional:
+Es un modelo de datos que representa la información en forma de tablas.
+
+Normalización:
+Es el proceso de organizar los datos en una base de datos relacional.
+
+Normalización:
+Es el proceso de organizar los datos en una base de datos relacional.
+
+Formas Normales:
+Son reglas que se utilizan para organizar los datos en una base de datos
+relacional.
+
+Primera Forma Normal (1FN):
+Es una forma normal que se utiliza para organizar los datos en una base de
+datos relacional.
+
+Segunda Forma Normal (2FN):
+Es una forma normal que se utiliza para organizar los datos en una base de
+datos relacional.
+
+Tercera Forma Normal (3FN):
+Es una forma normal que se utiliza para organizar los datos en una base de
+datos relacional.
+
+Cuarta Forma Normal (4FN o Boyce-Codd Normal Form - BCNF):
+Es una forma normal que se utiliza para organizar los datos en una base de
+datos relacional.
+
+Quinta Forma Normal (5FN o Domain-Key Normal Form - DKNF):
+Es una forma normal que se utiliza para organizar los datos en una base de
+datos relacional.
 */
