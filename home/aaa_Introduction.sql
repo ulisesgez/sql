570969d4657ce7ed35787ee107a6c42d332d2a05
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2024-03-14 10:00:53 -0600

    intro

---
+++ home/aaa_Introduction.sql
@@ -0,0 +1,58 @@
+/*
+Introducción a SQL:
+SQL es un lenguaje estándar para acceder y manipular bases de datos.
+
+¿Qué es SQL?
+SQL significa lenguaje de consulta estructurado
+SQL le permite acceder y manipular bases de datos
+SQL se convirtió en un estándar del Instituto Nacional Estadounidense de Estándares (ANSI)
+en 1986 y de la Organización Internacional de Normalización (ISO) en 1987.
+
+¿Qué puede hacer SQL?
+SQL puede ejecutar consultas en una base de datos
+SQL puede recuperar datos de una base de datos
+SQL puede insertar registros en una base de datos
+SQL puede actualizar registros en una base de datos
+SQL puede eliminar registros de una base de datos
+SQL puede crear nuevas bases de datos
+SQL puede crear nuevas tablas en una base de datos
+SQL puede crear procedimientos almacenados en una base de datos.
+SQL puede crear vistas en una base de datos
+SQL puede establecer permisos sobre tablas, procedimientos y vistas
+SQL es un estándar, PERO....
+Aunque SQL es un estándar ANSI/ISO, existen diferentes versiones del lenguaje SQL.
+
+Sin embargo, para cumplir con el estándar ANSI, todos admiten al menos los comandos
+principales (como,,,, ) de SELECTmanera similar .UPDATE DELETEINSERT WHERE
+
+Usando SQL en su sitio web
+Para crear un sitio web que muestre datos de una base de datos, necesitará:
+
+Un programa de base de datos RDBMS (es decir, MS Access, SQL Server, MySQL)
+Para utilizar un lenguaje de programación del lado del servidor, como PHP o ASP
+Para usar SQL para obtener los datos que desea
+Usar HTML/CSS para diseñar la página
+
+RDBMS
+RDBMS significa Sistema de gestión de bases de datos relacionales.
+
+RDBMS es la base de SQL y de todos los sistemas de bases de datos modernos, como MS SQL Server, IBM DB2, Oracle, MySQL y Microsoft Access.
+
+Los datos en RDBMS se almacenan en objetos de base de datos llamados tablas. Una tabla es una colección de entradas de datos relacionados y consta de columnas y filas.
+
+Mire la tabla "Clientes":
+
+SELECT * FROM Customers;
+
+Cada tabla se divide en entidades más pequeñas llamadas campos. Los campos de la tabla
+Clientes constan de ID de cliente, Nombre de cliente, Nombre de contacto, Dirección,
+Ciudad, Código postal y País. Un campo es una columna de una tabla diseñada para
+mantener información específica sobre cada registro de la tabla.
+
+Un registro, también llamado fila, es cada entrada individual que existe en una tabla.
+Por ejemplo, hay 91 registros en la tabla Clientes anterior. Un registro es una entidad
+horizontal en una tabla.
+
+Una columna es una entidad vertical en una tabla que contiene toda la información
+asociada con un campo específico en una tabla.
+*/
