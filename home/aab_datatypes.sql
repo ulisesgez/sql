9efea75e18376b678dc6cd61681693d24068c80a
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2024-03-16 09:47:42 -0600

    dataTypes

---
+++ home/aab_datatypes.sql
@@ -0,0 +1,9 @@
+/*
+Tipos de datos:
+Cada columna de una tabla de base de datos debe tener un nombre y un tipo de datos.
+
+Un desarrollador de SQL debe decidir qué tipo de datos se almacenarán dentro de cada
+columna al crear una tabla. El tipo de datos es una guía para que SQL comprenda qué
+tipo de datos se espera dentro de cada columna y también identifica cómo interactuará
+SQL con los datos almacenados.
+*/
