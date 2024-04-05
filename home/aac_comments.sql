75db14f59f8a2a9c2a1e639ebc291a42498c6b20
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2024-04-05 08:31:09 -0600

    text

--- home/aac_comments.sql
+++ home/aac_comments.sql
@@ -1,16 +1,16 @@
 /*
-COMENTAROS
- los comentarios se utilizan para documentar y explicar el código SQL.
- Los comentarios no son ejecutados por el motor de la base de datos y, por lo tanto,
- no tienen ningún impacto en la ejecución del código. Hay dos formas comunes de
- agregar comentarios en SQL:
+Comentarios:
+Los comentarios se utilizan para documentar y explicar el código SQL.
+Los comentarios no son ejecutados por el motor de la base de datos y, por lo tanto,
+no tienen ningún impacto en la ejecución del código. Hay dos formas comunes de
+agregar comentarios en SQL:
  
- Comentarios de línea única: Estos comentarios se utilizan para agregar explicaciones
- en una sola línea de texto.
+Comentarios de línea única: Estos comentarios se utilizan para agregar explicaciones
+en una sola línea de texto.
 
- Comentarios de varias líneas: Estos comentarios se utilizan para agregar explicaciones
- que abarcan varias líneas de texto.
- */
+Comentarios de varias líneas: Estos comentarios se utilizan para agregar explicaciones
+que abarcan varias líneas de texto.
+*/
 
 -- Este es un comentario de línea única
 
--- home/aad_select.sql
+++ home/aad_select.sql
@@ -1,6 +1,9 @@
 /*
-SELECT
-Lección 8: https://youtu.be/OuJerKzV5T0?t=5618
+Select:
+Es una de las cláusulas fundamentales en SQL y se utiliza para recuperar datos de
+una o más tablas de una base de datos. Con la cláusula "SELECT", puedes especificar
+qué columnas deseas recuperar y, opcionalmente, aplicar filtros y realizar operaciones
+en los datos seleccionados. La sintaxis básica de una consulta SELECT es la siguiente:
 */
 
 -- Obtiene todos los datos de la tabla "users"
