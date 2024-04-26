9856b04183e099d9396c5de48728634b16a055b6
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2024-04-25 22:06:06 -0600

    sum

--- home/aao_sum.sql
+++ home/aao_sum.sql
@@ -1,6 +1,7 @@
 /*
-SUM
-Lección 10.5: https://youtu.be/OuJerKzV5T0?t=8128
+Sum:
+se utiliza para calcular la suma de los valores en una columna específica.
+Puede ser útil para calcular totales o sumar valores numéricos en una tabla.
 */
 
 -- Suma todos los valores del campo edad de la tabla "users"
