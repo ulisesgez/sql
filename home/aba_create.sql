316b48029256c13fa6bf42fdb2681404974c7b0b
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2024-05-08 19:39:49 -0600

    createDrop

---
+++ home/aba_create.sql
@@ -0,0 +1,10 @@
+/*
+CREATE DATABASE
+se utiliza para crear varios tipos de objetos en una base de datos, como tablas,
+índices, vistas, procedimientos almacenados, funciones, y más.
+La sintaxis exacta y los parámetros requeridos pueden variar según el tipo de
+objeto que estés creando.
+*/
+
+-- Crea una base de datos llamada "test"
+CREATE DATABASE test;
---
+++ home/abb_drop.sql
@@ -0,0 +1,9 @@
+/*
+DROP DATABASE
+se utiliza para eliminar objetos de la base de datos, como tablas, bases de datos,
+índices, vistas, procedimientos almacenados, etc. Dependiendo del objeto que desees
+eliminar, usarás una variante específica de la sentencia DROP.
+*/
+
+-- Elimina la base de datos llamada "test"
+DROP DATABASE test;
